1eeb7566f328a3248dd0aca658667720f3511036
JP Deguzman <jddeguzman@ucdavis.edu>
2016-11-28 12:53:35 -0800

    Added a UIAlertController for users to submit answers with

--- Project5/ViewController.swift
+++ Project5/ViewController.swift
@@ -17,6 +17,8 @@ class ViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(promptForAnswer))
+        
         // Unwrap and find the path to a txt file named start using a built-in method of Bundle called path(forResource:)
         if let startWordsPath = Bundle.main.path(forResource: "start", ofType: "txt") {
             // Unwrap and create a string instance from the contents of a file at a particular path. If the code doesn't work, it will return a nil.
@@ -37,6 +39,25 @@ class ViewController: UITableViewController {
         title = allWords[0]
         usedWords.removeAll(keepingCapacity: true)
         tableView.reloadData()
+    }
+    
+    func promptForAnswer() {
+        // Create a UIAlertController instance to prompt user for an answer
+        let ac = UIAlertController(title: "Enter answer", message: nil, preferredStyle: .alert)
+        
+        // Add an editable text input field (unlike UILabel, which cannot be edited) to the UIAlertController
+        ac.addTextField()
+        
+        // This is a trailing closure. Everything before 'in' describes the closure and everything after it is the closure, thus the closure accepts one parameter of type UIAlertAction. '[unowned self, ac]' prevents creating a strong reference cycle because the closure does not own self and ac.
+        let submitAction = UIAlertAction(title: "Submit", style: .default) { [unowned self, ac] (action: UIAlertAction!) in
+            let answer = ac.textFields![0]
+            self.submit(answer: answer.text!)
+        }
+        
+        // Adds the UIAlertAction, submitAction, to the UIAlertController
+        ac.addAction(submitAction)
+        
+        present(ac, animated: true)
     }
     
     // Overriding these tableView methods help handle the table view data: numberOfRowsInSection and cellForRowAt
